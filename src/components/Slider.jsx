f836b03c591fa7df096898f2213dc1dc2fe8c5c8
shm_usuf <shmusuf@gmail.com>
2023-01-01 20:01:29 +0600

    slider added

---
+++ src/components/Slider.jsx
@@ -0,0 +1,38 @@
+const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/1787236/pexels-photo-1787236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/1444416/pexels-photo-1444416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 5,
+    src: "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
+
+const Slider = () => {
+  return (
+    <div className="slider">
+      {data.map((image) => (
+        <div
+          className="slide"
+          style={{ backgroundImage: `url(${image.src})` }}
+          key={image.id}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
