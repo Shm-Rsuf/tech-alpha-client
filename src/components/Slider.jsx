99f93a29468a635388f1706cfd82acf74d3a5991
shm_usuf <shmusuf@gmail.com>
2023-01-02 14:53:12 +0600

    slider added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+
 const data = [
   {
     id: 1,
@@ -22,15 +25,52 @@ const data = [
 ];
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const prevSlide = () => {
+    console.log("first");
+    setCurrentSlide(
+      currentSlide === 0 ? data.length - 1 : (prevSlide) => prevSlide - 1
+    );
+  };
+  const nextSlide = () => {
+    setCurrentSlide(
+      currentSlide === data.length - 1 ? 0 : (prevSlide) => prevSlide + 1
+    );
+  };
+
   return (
-    <div className="slider">
-      {data.map((image) => (
-        <div
-          className="slide"
-          style={{ backgroundImage: `url(${image.src})` }}
-          key={image.id}
-        ></div>
-      ))}
+    <div className="frame relative">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${100 * currentSlide}vw)` }}
+      >
+        {data.map((image) => (
+          <div
+            className="slide"
+            style={{ backgroundImage: `url(${image.src})` }}
+            key={image.id}
+          ></div>
+        ))}
+      </div>
+      <div className="btns absolute z-[1] text-gray-50 text-2xl flex gap-10 bottom-10 w-screen justify-center">
+        <button
+          onClick={prevSlide}
+          className="prev-btn h-10 w-14 bg-white/50 backdrop-blur-xl flex justify-center items-center hover:bg-white/90 hover:text-black duration-300"
+        >
+          <span>
+            <BsArrowLeft />
+          </span>
+        </button>
+        <button
+          onClick={nextSlide}
+          className="next-btn h-10 w-14 bg-white/50 backdrop-blur-xl flex justify-center items-center hover:bg-white/90 hover:text-black duration-300"
+        >
+          <span>
+            <BsArrowRight />
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
