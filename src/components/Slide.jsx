9f919fb8bf79649f5ef898cf5349f5eb3eea38fb
shm_usuf <shmusuf@gmail.com>
2023-01-02 23:06:05 +0600

    slider data added

---
+++ src/components/Slide.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Slide = ({ image }) => {
+  return (
+    <div className="slide" style={{ backgroundImage: `url(${image.src})` }}>
+      <div className="slide-texts container mx-auto flex flex-col justify-center items-start h-full gap-3">
+        <h1 className="space-font text-6xl text-violet-50 uppercase w-3/5">
+          {image.headline}
+        </h1>
+        <p className="text-xl w-2/5 text-violet-50/75 font-semibold">
+          {image.detail}
+        </p>
+        <Link className="slide-btn uppercase h-14 w-72 border-2 border-violet-50 py-2 px-4 duration-500 hover:text-orange-50 mt-5">
+          <span className="z-[2] absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-full text-center">
+            {image.cta}
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,26 +1,47 @@
 import { useState } from "react";
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import Slide from "./Slide";
 
 const data = [
   {
     id: 1,
     src: "https://images.pexels.com/photos/1787236/pexels-photo-1787236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "DSLR cameras for stunning photos",
+    detail:
+      "Are you an aspiring photographer looking to take your skills to the next level? Our DSLR cameras offer advanced features and high-quality image sensors to help you capture stunning photos. From landscape shots to portraits, these cameras are perfect for capturing all types of subjects.",
+    cta: "Shop DSLR cameras now",
   },
   {
     id: 2,
     src: "https://images.pexels.com/photos/1444416/pexels-photo-1444416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Upgrade your home entertainment with our TVs",
+    detail:
+      "Experience the latest in home entertainment with our selection of TVs. From sleek and modern designs to advanced features like 4K resolution and smart capabilities, our TVs will bring your favorite movies, TV shows, and streaming content to life.",
+    cta: "Shop TVs and upgrade now",
   },
   {
     id: 3,
     src: "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Enhance your listening experience",
+    detail:
+      "Take your music, movies, and more to the next level with our headphones. Our selection offers a range of styles and features, including noise-cancelling technology, wireless connectivity, and comfortable designs for all-day wear.",
+    cta: "Experience enhanced sound",
   },
   {
     id: 4,
     src: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Take your gaming to the next level",
+    detail:
+      "Elevate your gaming experience with our selection of gaming consoles. From the latest models to classic systems, we have a console for every type of gamer. Our consoles offer advanced graphics, fast processing speeds, and a variety of exclusive games to choose from.",
+    cta: "Shop consoles and play now",
   },
   {
     id: 5,
     src: "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    headline: "Stay connected with smart watches",
+    detail:
+      "Stay connected and on top of your day with our smart watches. Our selection offers a range of styles and features, including fitness tracking, phone notifications, and voice assistants. These watches are the perfect combination of functionality and style.",
+    cta: "Connect with a smart watch",
   },
 ];
 
@@ -46,17 +67,13 @@ const Slider = () => {
         style={{ transform: `translateX(-${100 * currentSlide}vw)` }}
       >
         {data.map((image) => (
-          <div
-            className="slide"
-            style={{ backgroundImage: `url(${image.src})` }}
-            key={image.id}
-          ></div>
+          <Slide image={image} key={image.id} />
         ))}
       </div>
       <div className="btns absolute z-[1] text-gray-50 text-2xl flex gap-10 bottom-10 w-screen justify-center">
         <button
           onClick={prevSlide}
-          className="prev-btn h-10 w-14 bg-white/50 backdrop-blur-xl flex justify-center items-center hover:bg-white/90 hover:text-black duration-300"
+          className="prev-btn h-10 w-14 flex justify-center items-center bg-violet-800 text-violet-200 border-2 border-violet-200 hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300 duration-300"
         >
           <span>
             <BsArrowLeft />
@@ -64,7 +81,7 @@ const Slider = () => {
         </button>
         <button
           onClick={nextSlide}
-          className="next-btn h-10 w-14 bg-white/50 backdrop-blur-xl flex justify-center items-center hover:bg-white/90 hover:text-black duration-300"
+          className="next-btn h-10 w-14 flex justify-center items-center bg-violet-800 text-violet-200  border-2 border-violet-200 hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300 duration-300"
         >
           <span>
             <BsArrowRight />
