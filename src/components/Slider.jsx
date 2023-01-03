35aff7d404a123ad2da7148961cfc43e20983c5e
shm_usuf <shmusuf@gmail.com>
2023-01-03 14:03:46 +0600

    hero added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,43 +5,48 @@ import Slide from "./Slide";
 const data = [
   {
     id: 1,
-    src: "https://images.pexels.com/photos/1787236/pexels-photo-1787236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://i.ibb.co/XszmG02/camera.jpg",
     headline: "DSLR cameras for stunning photos",
     detail:
       "Are you an aspiring photographer looking to take your skills to the next level? Our DSLR cameras offer advanced features and high-quality image sensors to help you capture stunning photos. From landscape shots to portraits, these cameras are perfect for capturing all types of subjects.",
     cta: "Shop DSLR cameras now",
+    category: "cameras",
   },
   {
     id: 2,
-    src: "https://images.pexels.com/photos/1444416/pexels-photo-1444416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://i.ibb.co/mtc8v16/tv.jpg",
     headline: "Upgrade your home entertainment with our TVs",
     detail:
       "Experience the latest in home entertainment with our selection of TVs. From sleek and modern designs to advanced features like 4K resolution and smart capabilities, our TVs will bring your favorite movies, TV shows, and streaming content to life.",
     cta: "Shop TVs and upgrade now",
+    category: "tvs",
   },
   {
     id: 3,
-    src: "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://i.ibb.co/kmr5qQv/headphones.jpg",
     headline: "Enhance your listening experience",
     detail:
       "Take your music, movies, and more to the next level with our headphones. Our selection offers a range of styles and features, including noise-cancelling technology, wireless connectivity, and comfortable designs for all-day wear.",
     cta: "Experience enhanced sound",
+    category: "headphones",
   },
   {
     id: 4,
-    src: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://i.ibb.co/JqxDhvZ/console.jpg",
     headline: "Take your gaming to the next level",
     detail:
       "Elevate your gaming experience with our selection of gaming consoles. From the latest models to classic systems, we have a console for every type of gamer. Our consoles offer advanced graphics, fast processing speeds, and a variety of exclusive games to choose from.",
     cta: "Shop consoles and play now",
+    category: "consoles",
   },
   {
     id: 5,
-    src: "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://i.ibb.co/YbS7mL2/smart-watches.jpg",
     headline: "Stay connected with smart watches",
     detail:
       "Stay connected and on top of your day with our smart watches. Our selection offers a range of styles and features, including fitness tracking, phone notifications, and voice assistants. These watches are the perfect combination of functionality and style.",
     cta: "Connect with a smart watch",
+    category: "smart-watches",
   },
 ];
 
@@ -73,7 +78,7 @@ const Slider = () => {
       <div className="btns absolute z-[1] text-gray-50 text-2xl flex gap-10 bottom-10 w-screen justify-center">
         <button
           onClick={prevSlide}
-          className="prev-btn h-10 w-14 flex justify-center items-center bg-violet-800 text-violet-200 border-2 border-violet-200 hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300 duration-300"
+          className="prev-btn h-10 w-14 flex justify-center items-center bg-violet-800 text-violet-200 border border-violet-200 hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300 duration-300"
         >
           <span>
             <BsArrowLeft />
@@ -81,7 +86,7 @@ const Slider = () => {
         </button>
         <button
           onClick={nextSlide}
-          className="next-btn h-10 w-14 flex justify-center items-center bg-violet-800 text-violet-200  border-2 border-violet-200 hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300 duration-300"
+          className="next-btn h-10 w-14 flex justify-center items-center bg-violet-800 text-violet-200  border border-violet-200 hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300 duration-300"
         >
           <span>
             <BsArrowRight />
