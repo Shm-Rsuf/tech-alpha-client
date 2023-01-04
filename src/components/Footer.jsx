ae4d20273cd905cad243d94ad4266decbfcaf43f
shm_usuf <shmusuf@gmail.com>
2023-01-04 12:28:18 +0600

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <div>
-      <h2>Footer</h2>
+      <h2 className=" footer bg-violet-700 text-violet-100 py-10 font-medium flex justify-center items-center">
+        <p>&copy; {new Date().getFullYear()} Tech Alpha. All rights reserved</p>
+      </h2>
     </div>
   );
 };
